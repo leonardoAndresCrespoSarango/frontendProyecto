8ef3f6106768b16f2a92604c45d0ebf68ebbb4a6
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2023-06-06 19:08:28 -0500

    frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./usuario/login/login.component";
 import {RegisterComponent} from "./usuario/register/register.component";
 import {DashboardComponent} from "./catalogo/dashboard/dashboard.component";
+import {InscripcionComponent} from "./recurso/inscripcion/inscripcion.component";
 
 
 const routes: Routes = [
   {path:"usuario/login", component: LoginComponent},
   {path:"usuario/register", component: RegisterComponent},
   {path:"catalogo/dashboard", component: DashboardComponent},
+  {path:"catalogo/cursos", component: InscripcionComponent},
 ];
 
 @NgModule({
