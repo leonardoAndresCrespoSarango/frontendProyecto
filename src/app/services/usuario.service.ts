711a02c29278c317e96a2a4bd700abd7494c4649
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2023-06-06 22:23:05 -0500

    frontend 20:05

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -7,6 +7,7 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class UsuarioService {
+  private API_SERVER="http://localhost:8001/usuario/usu/";
 
   constructor(private httpClient: HttpClient) { }
   save(cliente: Usuario): Observable<any[]> {
@@ -16,5 +17,11 @@ export class UsuarioService {
   updateCliente(cliente: Usuario): Observable<any[]> {
     //Para poner usamos el .post y para obtener el .get
     return this.httpClient.post<any>("http://localhost:8001/usuario/actualizar/{id}", cliente);
+  }
+  buscar(cliente: Usuario): Observable<any[]> {
+    var valor=cliente.usu;
+    var con=cliente.contrasenia
+    return this.httpClient.get<Usuario[]>("http://localhost:8001/usuario/usu/"+valor+"/contrasenia/"+con);
+
   }
 }
--- src/app/usuario/login/login.component.ts
+++ src/app/usuario/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Usuario} from "../../domain/persona";
+import {Router} from "@angular/router";
+import {UsuarioService} from "../../services/usuario.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  productosLista = new Array()
+  editar=0;
+  cliente: Usuario = new Usuario()
+  constructor(private router: Router,private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
   }
+  buscarPersona() {
+    console.log(this.cliente);
+    this.usuarioService.buscar(this.cliente).subscribe((data: any[]) => {
+      console.log(data);
+      this.productosLista = data;
+      this.router.navigate(["catalogo/dashboard"])
+
+    }, (error) => {
+      console.error(error);
+      window.alert('credenciales incorrectas');
+    });
+  }
+
 
 }
